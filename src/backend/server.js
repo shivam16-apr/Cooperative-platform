404a1c3f086018207bf06ec1ff188fce6e97ad2a
Aakansha Agrawal <aakanshaagrawal0002@gmail.com>
2026-09-10 23:02:21 +0530

    Fix backend deployment

--- src/backend/server.js
+++ src/backend/server.js
@@ -44,7 +44,8 @@ app.get("/api/health", async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.get("/api/protected", authenticateToken, (req, res) => {
     res.json({
         message: "You accessed a protected route!",
@@ -52,6 +53,6 @@ app.get("/api/protected", authenticateToken, (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
